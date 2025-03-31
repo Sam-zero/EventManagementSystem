2fde30c8b53a5f9b8a8ed201594bc18da9d1851d
Sam-zero <zharylkapsamat@gmail.com>
2025-03-31 18:29:43 +0500

    Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,6 @@ class Participant {
     public Participant(String name, int age, String email) {
         this.name = name;
         this.age = age;
-        this.email = email;
     }
 
     public String getName() {
